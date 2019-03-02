252c7c2e7501ebf5f1d60750089c35b0856fd1f6
Jolo DeGuzman <jolo.deguzm4n@gmail.com>
2019-03-02 12:59:35 -0800

    Set color to -

--- src/main/java/io/battlesnake/starter/Snake.java
+++ src/main/java/io/battlesnake/starter/Snake.java
@@ -107,7 +107,7 @@ public class Snake {
          */
         public Map<String, String> start(JsonNode startRequest) {
             Map<String, String> response = new HashMap<>();
-            response.put("color", "#ff00ff");
+            response.put("color", "#736CCB");
             return response;
         }
 
